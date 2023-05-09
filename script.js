02b565023184194d5f6812d7c6bdf8b508695e62
Melissa Webster <imelissawebster@gmail.com>
2023-05-09 12:45:32 -0300

    javascript setup

---
+++ script.js
@@ -0,0 +1,16 @@
+function readMore() {
+    const division = document.querySelector('span#division')
+    const moreText = document.querySelector('span#more-text')
+    const moreButton = document.querySelector('button#more-button')
+
+    if (division.style.display === "none") {
+        division.style.display = "inline";
+        moreButton.innerHTML = "read more..."; 
+        moreText.style.display = "none";
+      } else {
+        division.style.display = "none";
+        moreButton.innerHTML = "read less"; 
+        moreText.style.display = "inline";
+      }
+
+}
