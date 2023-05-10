2df66809d8bc989a6a6c47062fc68f9d85a75b20
Melissa Webster <imelissawebster@gmail.com>
2023-05-10 16:03:30 -0300

    js function enhance

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function readMoreLess() {
-    let moreText = document.getElementById('more-text')
-    let moreButton = document.getElementsByTagName('button')[0]
 
-    if (moreText.style.display === "none") {
-        moreText.style.display = "block";
-        moreButton.innerHTML = "Read less";
-      } else {
-        moreText.style.display = "none";
-        moreButton.innerHTML = "Read more...";
-      }
+
+function showMoreLess(event, button) {
+  const container = button.parentNode
+  const addText = container.querySelector('.hidden-text')
+  if (addText.classList == 'hidden-text') {
+    button.innerHTML = 'Read less'
+    addText.classList.toggle('show')
+  } else {
+    button.innerHTML = 'Read more...'
+    addText.classList.toggle('show')
+  }
 }
 
