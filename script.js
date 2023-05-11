4eeb69bbbc2b74ccfb4a2b8c002deef4c9aeb662
Melissa Webster <imelissawebster@gmail.com>
2023-05-11 19:52:43 -0300

    add additional texts

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function showMoreLess(event, button) {
   const container = button.parentNode
   const addText = container.querySelector('.hidden-text')
   if (addText.classList == 'hidden-text') {
-    button.innerHTML = 'Read less'
+    button.innerHTML = 'read less'
     addText.classList.toggle('show')
   } else {
-    button.innerHTML = 'Read more...'
+    button.innerHTML = 'read more...'
     addText.classList.toggle('show')
   }
 }
